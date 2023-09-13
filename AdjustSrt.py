7d2804261a4cead8f86c7fd73b3c9b19850947e4
zj1123581321 <33171229+zj1123581321@users.noreply.github.com>
2023-09-13 12:24:31 +0800

    修复了时间戳合并后次序错乱的问题

--- AdjustSrt.py
+++ AdjustSrt.py
@@ -1,7 +1,11 @@
 import re
 
+# 定义需要转写的 srt 文件位置,# 输出文件保持源文件相同路径，文件名为 adjusted_源文件名
+srt_file = 'D:/OneDrive/HR HK Lessons/5140 Statistical Methods in Quantitative Finance/week1/5140 week1 音频.srt'
+output_file = 'D:/OneDrive/HR HK Lessons/5140 Statistical Methods in Quantitative Finance/week1/adjusted_5140 week1 音频.srt'
+
 # 读取srt文件内容
-with open('subtitle.srt', 'r') as file:
+with open(srt_file, 'r') as file:
     content = file.read()
 
 # 将内容按空行分割成不同的组
@@ -46,5 +50,5 @@ for i in range(len(old_groups)):
             index += 1
 
 # 将new_groups的内容按srt格式写入新的srt文件
-with open('new_subtitle.srt', 'w') as file:
+with open(output_file, 'w') as file:
     file.write('\n\n'.join(new_groups))
