749c022932d9cc80b714563066005338267c82ec
zj1123581321 <33171229+zj1123581321@users.noreply.github.com>
2023-09-17 19:08:35 +0800

    增加了根据每行字幕前 1/3 内容分段的功能

--- AdjustSrt.py
+++ AdjustSrt.py
@@ -4,15 +4,16 @@ import os
 # 每行字幕的最短字数，adjust_mode 为 3 时有效
 min_length = 120
 max_length = 180
+if_need_spilt = True # 是否需要根据非逗号拆分字幕，时间戳根据字符长度比例拆分，并不一定准确。实验性功能，建议在连续多句字幕都无标点结尾时使用。
 
 
 def main():
-    srt_file = 'D:/MyFolders/Developments/0Python/230912_AdjustSubTitle/subtitle.srt'
+    srt_file = 'D:/OneDrive/HR HK Lessons/5030 Quantitative Modeling of Derivatives Securities/week3/HKUST Canvas - MAFS5030-L1.srt'
     # 字幕的调整方式，
     # 1 为合并被断行的句子，
     # 2 在 1 的基础上，保证每行以非逗号结尾
     # 3 在 1-2 的基础上，保证每一行的字数不小于目标值
-    adjust_mode = '1'
+    adjust_mode = '3'
     adjust_srt_file(srt_file, adjust_mode)
 
 def generate_output_file_path(srt_file):
@@ -32,6 +33,65 @@ def read_and_split_srt_file(srt_file):
     # 去除 old_groups 中的空字符串
     old_groups = [group for group in old_groups if group != '']
     return old_groups
+
+# 拆分每行字幕。如果一行字幕的 1/3 长度内容里含有非逗号的标点，则拆分成两行字幕。同时时间戳按照开头到标点的长度占总长度的比例进行拆分。
+def split_srt_content(old_groups):
+    # 用于记录当前该处理 old_groups 中的第几个组
+    current_number = 0
+    # 用于在 new_groups 中的索引
+    index = 0
+    new_groups = []
+
+    for i in range(len(old_groups)):
+        # 如果 i 不等于 current_number 就跳过
+        if(i == current_number):
+            # 提取编号、时间段和字幕文本
+            group = old_groups[i].strip().split('\n')
+
+            if len(group) >= 3:
+                time_range = group[1]
+                text = ' '.join(group[2:])
+                # 检查当前字幕的前 1/3 内容是否含有非逗号的标点，如果有则拆分成两行字幕。
+                text_1_3 = text[:int(len(text)/3)]
+                # 如果当前字幕的前 1/3 内容有除了逗号以外的标点，则拆分成两行字幕
+                if re.search(r'[^\w,，\s]', text_1_3):
+                    # 获取当前字幕的前 1/3 内容的最后一个标点符号的索引
+                    last_punctuation_index = re.search(r'[^\w,，\s]', text_1_3).span()[1]
+                    # print(i, last_punctuation_index)
+                    # 根据最后一个标点将当前字幕拆分成两行字幕
+                    text_1 = text[:last_punctuation_index]
+                    text_2 = text[last_punctuation_index:]
+                    # 计算原字幕的持续时间，然后根据拆分后的两行字幕的长度占原字幕长度的比例，计算出两行字幕的持续时间。
+                    time_range_1, time_range_2 = split_time_range(time_range, len(text_1) / len(text))
+                    new_groups.append(f"{index}\n{time_range_1}\n{text_1}")
+                    new_groups.append(f"{index+1}\n{time_range_2}\n{text_2}")
+                    current_number += 1
+                    index += 2
+                else:
+                    new_groups.append(f"{index}\n{time_range}\n{text}")
+                    current_number += 1
+                    index += 1
+    return new_groups
+
+# 拆分时间段的函数，输入原时间段和拆分比例，返回拆分后的时间段。中间过程精确到毫秒。
+def split_time_range(time_range, split_ratio):
+    # 获取原时间段的开始时间和结束时间
+    time_range_start = re.search(r'(\d{2}:\d{2}:\d{2},\d{3})', time_range).group(1)
+    time_range_end = re.findall(r'(\d{2}:\d{2}:\d{2},\d{3})', time_range)[1]
+    # 计算原时间段的持续时间，然后根据拆分后的两行字幕的长度占原字幕长度的比例，计算出两行字幕的持续时间。此处需要注意 00:06:33,319 中有毫秒。
+    time_range_start_seconds = int(time_range_start.split(':')[0]) * 3600 + int(time_range_start.split(':')[1]) * 60 + int(time_range_start.split(':')[2].split(',')[0]) + int(time_range_start.split(':')[2].split(',')[1]) / 1000
+    time_range_end_seconds = int(time_range_end.split(':')[0]) * 3600 + int(time_range_end.split(':')[1]) * 60 + int(time_range_end.split(':')[2].split(',')[0]) + int(time_range_end.split(':')[2].split(',')[1]) / 1000
+    # 计算出拆分的时间，转换为 00:00:00,000 的格式
+    split_seconds = (time_range_end_seconds - time_range_start_seconds) * split_ratio
+    split_time = time_range_start_seconds + split_seconds
+    split_time_hour = int(split_time / 3600)
+    split_time_minute = int((split_time - split_time_hour * 3600) / 60)
+    split_time_second = int(split_time - split_time_hour * 3600 - split_time_minute * 60)
+    split_time_millisecond = int((split_time - split_time_hour * 3600 - split_time_minute * 60 - split_time_second) * 1000)
+    split_time_range = f"{split_time_hour:02d}:{split_time_minute:02d}:{split_time_second:02d},{split_time_millisecond:03d}"
+    # 返回两个拼接后的时间端 time_range_start -> split_time_range 和 split_time_range -> time_range_end
+    return f"{time_range_start} --> {split_time_range}", f"{split_time_range} --> {time_range_end}"
+
 
 # 把断句合并成一句
 def adjust_srt_content(old_groups):
@@ -174,6 +234,9 @@ def write_new_srt_file(output_file, new_groups):
 
 def adjust_srt_file(srt_file, adjust_mode):
     old_groups = read_and_split_srt_file(srt_file)
+    if if_need_spilt:
+        print('adjust_mode is 0，先根据非逗号拆分行')
+        old_groups = split_srt_content(old_groups)
     # 断句合并是基础操作
     new_groups = adjust_srt_content(old_groups)
     if adjust_mode == '2':
